fe0ed044d70cdde2eb0f5e93b34d117b9dbe2b7d
Miguel Patino <mpatino06@hotmail.com>
2021-04-05 09:13:24 -0500

    Interface and Product Controller

--- ProductWEB/Startup.cs
+++ ProductWEB/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProductWEB.Repository;
+using ProductWEB.Repository.IRepository;
 
 namespace ProductWEB
 {
@@ -23,6 +25,8 @@ namespace ProductWEB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient();
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
